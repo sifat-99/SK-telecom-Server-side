6fdb6d1d9f31ea8edf69e77b0ab8faae46e4cb59
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-10-18 13:50:13 +0600

    server side initialized and index.js file created, .gitignore addeded

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5001;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+
+    
+
+
+// listen
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+    });
