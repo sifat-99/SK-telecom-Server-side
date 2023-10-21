3e2ba8ed4f5e74125b41b3eae776bf9e8afbb6ca
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-10-21 16:57:45 +0600

    await client.connect() comment out for fast load data

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
 
 
@@ -101,7 +101,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
